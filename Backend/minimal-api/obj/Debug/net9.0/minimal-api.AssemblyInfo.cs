598afbe9ce44d3fdf129b76708b5d478832bdf94
Andre Prohmann <98752890+andreprohmann@users.noreply.github.com>
2025-10-13 22:07:45 -0300

    Update build output and project directory paths
    Artefatos de compilação regenerados com a alteração do diretório do projeto de "backend" para "Backend" no caminho. A versão informativa do assembly e os arquivos de cache relacionados também foram atualizados como parte do processo de compilação.

--- Backend/minimal-api/obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ Backend/minimal-api/obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8b32d365dd5c8a863106ebe640d22a025bef1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef932c5aaf5f63181addcfcf46c6c4360fcbbeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
